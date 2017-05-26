e3e7f84c90c525b46d86a64d2ed91c7f9a66628c
logonrm <logonrm@fiap.com.br>
2017-05-26 08:48:37 -0300

    Inclusao do projeto

---
+++ app/src/main/java/com/example/logonrm/pizzaria/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.logonrm.pizzaria;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etLogin;
+    private EditText etSenha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etLogin = (EditText) findViewById(R.id.etLogin);
+        etSenha = (EditText) findViewById(R.id.etSenha);
+
+    }
+
+    public void logar(View view){
+        String login = etLogin.getText().toString();
+        String senha = etSenha.getText().toString();
+        if (login.equals("FIAP") && senha.equals("123")){
+            Intent intent = new Intent(this, PedidoActivity.class);
+            intent.putExtra("usuario",login);
+            startActivity(intent);
+            finish();
+        }else{
+            Toast.makeText(this, "Usuário ou senha inválidos",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
